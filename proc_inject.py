87def2bef7749fdba22b6296d4fbecdd90a50cef
Dor <39267196+dor-alt@users.noreply.github.com>
2018-08-13 02:17:05 +0300

    Add files via upload

--- proc_inject.py
+++ proc_inject.py
@@ -3,7 +3,7 @@ from py_process_injection import inject
 
 
 def parse_command_line_arguments():
-    parser = OptionParser("usage: %prog -p 123 -d 'C:\Users\pc\\bad.dll\nPlease make sure you use a 32 bit process to inject to and 32 bit dll")
+    parser = OptionParser("usage: %prog -p 123 -d 'C:\Users\pc\\bad.dll\nPlease make sure your injected process is a 32 bit process and your given dll is a 32 bit dll")
     parser.add_option("-p", dest="pid", help="PID of the process to inject")
     parser.add_option("-d", dest="dll_path", help="The path of the dll to inject to the remote process")
     (options, args) = parser.parse_args()
