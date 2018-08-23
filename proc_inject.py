6c01f5e0d6c1b827524447be773c52946e2d2d14
Dor <39267196+dor-alt@users.noreply.github.com>
2018-08-23 22:00:24 +0300

    Add files via upload

--- proc_inject.py
+++ proc_inject.py
@@ -6,7 +6,7 @@ def parse_command_line_arguments():
     parser = OptionParser("usage: %prog -p 123 -d 'C:\Users\pc\\bad.dll -i 1\nPlease make sure your injected process is a 32 bit process and your given dll is a 32 bit dll")
     parser.add_option("-p", dest="pid", help="PID of the process to inject")
     parser.add_option("-d", dest="dll_path", help="The path of the dll to inject to the remote process")
-    parser.add_option("-i", dest="injection_type", help="Injection type of the chosen dll\n1 - Injecting the path of the dll to memory ( using LoadLibrary() )\n2 - Reflactive injection of the dll binary to memory")
+    parser.add_option("-i", dest="injection_type", help="Injection type of the chosen dll\n1 - Injecting the path of the dll to memory ( using LoadLibrary() )\n2 - Reflective injection of the dll binary to memory")
     (options, args) = parser.parse_args()
 
     if not options.pid or not options.dll_path or not options.injection_type:
