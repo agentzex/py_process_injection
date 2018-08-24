b66c42a0b6acab98a468db93d96114b74953b894
Dor <39267196+dor-alt@users.noreply.github.com>
2018-08-24 16:41:19 +0300

    Added the wrapper

--- injector.py
+++ injector.py
@@ -4,14 +4,18 @@ import psutil
 
 
 def inject_dll(pid, dll_path, injection_type):
-    validate(pid, dll_path, injection_type)
+    if not isinstance(injection_type, int):
+        raise Exception("The injection type given must be an int, not a string")
+    if injection_type != 1 and injection_type != 2:
+        raise Exception("Injection type given must be 1 or 2 as an int")
+
     if injection_type == 1:
         return inject_with_LoadLibrary(pid, dll_path)
     else:
         return inject_reflective(pid, dll_path)
 
 
-def validate(pid, dll_path, injection_type):
+def validate(pid, dll_path):
     try:
         dll_path = str(dll_path)
     except Exception , e:
@@ -30,17 +34,14 @@ def validate(pid, dll_path, injection_type):
     if not process_found:
         raise Exception("The PID given is not running on this machine")
 
-    if not isinstance(injection_type, int):
-        raise Exception("The injection type given must be an int, not a string")
-    if injection_type != 1 and injection_type != 2:
-        raise Exception("Injection type given must be 1 or 2 as an int")
-
 
 def inject_with_LoadLibrary(pid, dll_path):
+    validate(pid, dll_path)
     ret = inject(pid, dll_path, 1)
     return ret
 
 
 def inject_reflective(pid, dll_path):
+    validate(pid, dll_path)
     ret = inject(pid, dll_path, 2)
     return ret
